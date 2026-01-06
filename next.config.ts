0cf282494c926f98123e02354a115cd3a8e8f13d
dev854 <jahae854@gmail.com>
2026-01-06 19:03:14 +0900

    fix: update Supabase hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "hkmczijydgcssuknptdl.supabase.co",
+        hostname: "bazcqvuucqassrbpgtqj.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
     ],
